9e82b60d999cad9c618d86bf792fa75cbc14be40
Sergey Melekhin <sergey@melekhin.me>
2016-03-18 12:06:53 +1000

    initial commit

---
+++ src/components/ListManager.jsx
@@ -0,0 +1,26 @@
+var React = require("react");
+var List = require("./List.jsx");
+
+var ListManager = React.createClass({
+    getInitialState: function() {
+      return {items: [], newItemText: ""};
+    },
+    handleSubmit: function(e) {
+      e.preventDefault();
+      var currentItems = this.state.items;
+      currentItems.push(this.state.newItemText);
+      this.setState({items: this.state.items, newItemText: ""});
+    },
+    render: function() {
+      return (
+        <div>
+          <h3>{this.props.title}</h3>
+          <form onSubmit={this.handleSubmit}>
+            <input onChange={this.onChange} value={this.state.newItemText}/>
+          </form>
+        </div>
+      );
+    }
+});
+
+module.exports = ListManager;
