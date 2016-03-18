74d1e12d0db2f8bb731498b6e84a688b37924273
Sergey Melekhin <sergey@melekhin.me>
2016-03-18 12:42:19 +1000

    Fix/ListManager wrong setState

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,8 +3,12 @@ var ListItem = require("./ListItem.jsx");
 
 var List = React.createClass({
     render: function() {
+      var createItem = function(text, index) {
+        return <ListItem key={index+text} text={text} />;
+      };
       return (
-        <ul>        
+        <ul>
+          {this.props.items.map(createItem)}
         </ul>
       );
     }
