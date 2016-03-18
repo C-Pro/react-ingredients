59b4224f99e5f47b93ea191da57a4ca80be1d12c
Sergey Melekhin <sergey@melekhin.me>
2016-03-18 20:42:53 +1000

    Styled with bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,5 @@ var ReactDOM  = require("react-dom");
 var ListManager = require("./components/ListManager.jsx");
 
 ReactDOM.render(<ListManager title= "Ingredients" />, document.getElementById("ingredients"));
+ReactDOM.render(<ListManager title= "Todo" />, document.getElementById("todo"));
+ReactDOM.render(<ListManager title= "Tada" headingColor="#b31217" />, document.getElementById("tada"));
